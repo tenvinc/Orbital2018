8c01610460965eece7ac79ce9d064dd790122880
Vincent <vencintgamer@gmail.com>
2018-06-11 11:31:58 +0800

    Added drag and drop
    Haven't link up with the code behaviour yet(visual programming)

---
+++ Orbital2018/Assets/Drag.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public Transform originalParent = null;
+	public Transform dummyParent = null;
+
+	public GameObject dummyPrefab;
+	public GameObject myPrefab;
+
+	private Transform dummy = null;
+	private Transform clone = null;
+
+	public int originalIndex;
+    public int dummyIndex;
+	public float threshold = 100f;
+	public bool inConsole = false;
+
+	public void OnBeginDrag(PointerEventData data) {
+		originalParent = transform.parent;
+		originalIndex = transform.GetSiblingIndex();
+		//Debug.Log("Start dragging from " + originalParent.parent.name);
+        transform.SetParent(originalParent.parent, false);
+		if (!inConsole) {
+			clone = Instantiate(myPrefab, transform.position, transform.rotation).transform;
+			clone.SetParent(originalParent, false);
+			clone.SetSiblingIndex(originalIndex);	
+		}
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+	}
+
+	public void OnDrag(PointerEventData data) {
+        transform.position = data.position;
+		if (dummyParent != null) {
+			if (dummy == null) {
+				dummy = Instantiate(dummyPrefab, transform.position, transform.rotation).transform;
+				dummyIndex = -1;
+			}
+            dummy.transform.SetParent(dummyParent, false);
+			// Find nearest match
+			float minDist = Mathf.Infinity;
+			int indexToSwap = dummyIndex;
+			Transform transformToSwap = dummy.transform;
+			foreach (Transform c in dummyParent) {
+				float distDiff = Vector2.Distance(c.position, transform.position);
+				if (distDiff < minDist) {
+					minDist = distDiff;
+					indexToSwap = c.GetSiblingIndex();
+					transformToSwap = c;
+				}
+			}
+			if (minDist > threshold) return;
+			transformToSwap.SetSiblingIndex(dummy.GetSiblingIndex());
+			dummy.transform.SetSiblingIndex(indexToSwap);
+			if (dummyIndex == -1) 
+				dummyIndex = dummyParent.childCount;
+			else 
+				dummyIndex = indexToSwap;
+		}
+		else if (dummyParent == null && dummy != null) {
+			Debug.Log("Out of bound. Destroying dummy now");
+			dummyIndex = -1;
+			Destroy(dummy.gameObject);
+		}
+	}
+
+	public void OnEndDrag(PointerEventData data) {
+		if (dummyParent == null) {
+			Debug.Log("Lost connection, destruction starting now");
+			Destroy(gameObject);
+			return;
+		}
+		else {
+			Destroy(dummy.gameObject);
+			transform.SetParent(dummyParent, false);
+			transform.SetSiblingIndex(dummyIndex);
+			GetComponent<CanvasGroup>().blocksRaycasts = true;
+			inConsole = true;
+		}
+	}
+
+	void InitializeCode() {
+		
+	}
+}
---
+++ Orbital2018/Assets/DropZone.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+	public void OnPointerEnter(PointerEventData data) {
+        if (data.pointerDrag == null) return;
+        Drag d = data.pointerDrag.gameObject.GetComponent<Drag>();
+        if (d != null) {
+            d.dummyParent = transform;
+        }
+        Debug.Log("Entering zone");
+    }
+
+	public void OnPointerExit(PointerEventData data) {
+        if (data.pointerDrag == null) return;
+        Drag d = data.pointerDrag.gameObject.GetComponent<Drag>();
+        if (d != null) {
+            d.dummyParent = null;
+            d.dummyIndex = -1;
+        }
+        Debug.Log("Exiting zone");
+    }
+
+	 public void OnDrop(PointerEventData data) {
+        Debug.Log("Dropping on " + gameObject.name);
+    }
+}
