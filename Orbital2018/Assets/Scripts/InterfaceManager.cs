fe8a22a39c2c045f1e8fd3ef42523069320011de
Vincent <vencintgamer@gmail.com>
2018-06-11 22:34:52 +0800

    Revert "Revert "Added UI pause and start""
    This reverts commit ee89850d6b723462d4a5077c5bb96e851d904b97.

--- Orbital2018/Assets/Scripts/InterfaceManager.cs
+++ Orbital2018/Assets/Scripts/InterfaceManager.cs
@@ -2,15 +2,69 @@
 
 public class InterfaceManager : MonoBehaviour {
 
-    public static InterfaceManager ui;
     public Transform visibleUI;
     public Transform hiddenUI;
+    public static InterfaceManager ui;
+    public static bool gamePaused;
+
+    private bool stageStarted;
 
     void Awake() {
         if (ui != null) {
-            Debug.Log("More than 1 build manager in scene");
+            Debug.Log("More than 1 UI manager in scene");
         }
         else ui = this;
+    }
+
+    void Start() {
+        gamePaused = true;
+        stageStarted = false;
+        Time.timeScale = 0f;
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (gamePaused) {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+    }
+
+    public void StartStage() {
+        if (stageStarted) return;
+        stageStarted = true;
+        gamePaused = false;
+        Time.timeScale = 1f;
+    }
+
+    void Resume() {
+        if (!stageStarted) {
+            Debug.Log("Stage not yet started");
+            return;
+        }
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+    void Pause() {
+        if (!stageStarted) {
+            return;
+        }
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    public void TogglePause() {
+        if (!stageStarted) return;
+        if (gamePaused) {
+            Resume();
+        }
+        else {
+            Pause();
+        }
     }
 
 }
