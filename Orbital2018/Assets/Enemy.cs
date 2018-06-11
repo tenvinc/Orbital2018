2aeaac433a9c00dbab65ed51d7ce7bcef26d649b
shouteck <shouteckie@gmail.com>
2018-06-11 15:19:40 +0800

    shouteck 02
    - Added Healthbars to enemies

--- Orbital2018/Assets/Enemy.cs
+++ Orbital2018/Assets/Enemy.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour {
+
+    public float startHealth = 100f;
 
     [Header("Enemy Attributes")]
     public float speed = 60f;
     public float health = 100f;
     public int value = 50;
 
+    [Header("Unity Stuff")]
+    public Image healthBar;
+
+    void Start()
+    {
+        health = startHealth;
+    }
+
     public void TakeDamage (float damage)
     {
         health -= damage;
+        healthBar.fillAmount = health / startHealth;
         if (health <= 0)
             Die();
     }
