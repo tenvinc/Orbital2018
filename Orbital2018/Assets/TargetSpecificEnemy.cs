5eb64914007bc1d6e95ce568cd1c6b4cfba576c3
shouteck <shouteckie@gmail.com>
2018-06-13 06:28:48 +0800

    shouteck 03
    - Finished a start menu
    - Added a new script TargetSpecificEnemy (Work in progress)

---
+++ Orbital2018/Assets/TargetSpecificEnemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class TargetSpecificEnemy : BasicCode {
+
+    public string enemyTag;
+
+    public override void Run()
+    {
+        TSE(enemyTag);
+    }
+
+    void TSE (string enemyTag)
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(TagManager.tm.enemyTag);
+        float minDist = Mathf.Infinity;
+        Transform target = null;
+        for (int i = 0; i < enemies.Length - 1; i++)
+        {
+            float dist = Vector3.Distance(enemies[i].transform.position, towerRef.position);
+            if (dist < minDist)
+            {
+                minDist = dist;
+                target = enemies[i].transform;
+            }
+        }
+        towerRef.GetComponent<TurretShooting>().SetTarget(target, minDist);
+    }
+}
