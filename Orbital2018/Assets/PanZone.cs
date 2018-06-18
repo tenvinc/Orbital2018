d4b7d2d84249e657697987494ec0c9c2e769a41d
Vincent <vencintgamer@gmail.com>
2018-06-19 00:05:09 +0800

    Changed the way camera pans

---
+++ Orbital2018/Assets/PanZone.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PanZone : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public string panType = "East";
+    public bool inZone = false;
+    private CameraController mainCam;
+
+    [Header("Cardinal Directions")]
+    public string North = "North";
+    public string South = "South";
+    public string East = "East";
+    public string West = "West";
+
+
+    void Awake()
+    {
+        GameObject[] camList = GameObject.FindGameObjectsWithTag("MainCamera");
+        mainCam = camList[0].GetComponent<CameraController>();
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        inZone = true;
+        Debug.Log("InZone");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        inZone = false;
+        Debug.Log("OutZone");
+    }
+
+    void Update()
+    {
+        if (inZone)
+        {
+            if (panType == "North")
+            {
+                mainCam.PanUp();
+            }
+            else if (panType == "South")
+            {
+                mainCam.PanDown();
+            }
+            else if (panType == "West")
+            {
+                mainCam.PanLeft();
+            }
+            else if (panType == "East")
+            {
+                mainCam.PanRight();
+            }
+        }
+    }
+}
--- Orbital2018/Assets/Scripts/GameObject Scripts/CameraController.cs
+++ Orbital2018/Assets/Scripts/GameObject Scripts/CameraController.cs
@@ -22,26 +22,46 @@ public class CameraController : MonoBehaviour {
         if (!freeToPan) 
             return;
         // TODO: Add clamps to clamp the x,y,z for the player
-        if (Input.GetAxis("Vertical") > 0 || Input.mousePosition.y >= Screen.height - thresholdFromBorder) {
-            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        if (Input.GetAxis("Vertical") > 0) {
+            PanUp();
         }
-        else if (Input.GetAxis("Vertical") < 0 || Input.mousePosition.y <= thresholdFromBorder) {
-            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        else if (Input.GetAxis("Vertical") < 0) {
+            PanDown();
         }
-        else if (Input.GetAxis("Horizontal") < 0 || Input.mousePosition.x <= thresholdFromBorder) {
-            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        else if (Input.GetAxis("Horizontal") < 0) {
+            PanLeft();
         }
-        else if (Input.GetAxis("Horizontal") > 0 || Input.mousePosition.x >= Screen.width - thresholdFromBorder) {
-            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        else if (Input.GetAxis("Horizontal") > 0) {
+            PanRight();
         }
 
         float scrollVal = Input.GetAxis("Mouse ScrollWheel");
         float absScrollVal = Mathf.Abs(scrollVal);
         // Scroll wheel to zoom in and out
-        if (scrollVal > 0)
+        if (scrollVal > 0) 
             transform.Translate(Vector3.forward * absScrollVal * zoomSpeed * 1000 * Time.deltaTime);
         else if (scrollVal < 0)
             transform.Translate(Vector3.back * absScrollVal * zoomSpeed * 1000 * Time.deltaTime);
 
+    }
+
+    public void PanUp()
+    {
+        transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+    }
+
+    public void PanDown()
+    {
+        transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+    }
+
+    public void PanLeft()
+    {
+        transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+    }
+
+    public void PanRight()
+    {
+        transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
     }
 }
