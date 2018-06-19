e73191f18dc2d9facce0217df7c705ce00bf0d83
Vincent <vencintgamer@gmail.com>
2018-06-19 19:30:05 +0800

    Added visual cues for panning

--- Orbital2018/Assets/PanZone.cs
+++ Orbital2018/Assets/PanZone.cs
@@ -5,9 +5,6 @@ public class PanZone : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
 
     public string panType = "East";
-    private bool inZone;
-    private bool cameraLocked;
-    private CameraController mainCam;
 
     [Header("Cardinal Directions")]
     public string North = "North";
@@ -16,21 +13,31 @@ public class PanZone : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     public string West = "West";
 
 
+    private bool inZone;
+    private bool cameraLocked;
+    private CameraController mainCam;
+    private Transform image;
+
+
     void Awake()
     {
         GameObject[] camList = GameObject.FindGameObjectsWithTag("MainCamera");
         mainCam = camList[0].GetComponent<CameraController>();
         inZone = false;
         cameraLocked = false;
+        image = transform.GetChild(0);
+        image.gameObject.SetActive(false);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        if (!cameraLocked) image.gameObject.SetActive(true);
         inZone = true;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        if (!cameraLocked) image.gameObject.SetActive(false);
         inZone = false;
     }
 
